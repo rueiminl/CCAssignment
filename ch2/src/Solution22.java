fe28dd99cf4612070812668bf227be2552bd11db
rueiminl <rueiminl@andrew.cmu.edu>
2015-09-13 09:11:23 -0400

    2.2 Return Kth to Last: add main()

--- ch2/src/Solution22.java
+++ ch2/src/Solution22.java
@@ -8,7 +8,13 @@ public class Solution22 {
 		public int value;
 		public SinglyLinkedList next;
 	}
-	public static SinglyLinkedList ReturnKthToLast(SinglyLinkedList head, int k) {
+	
+	/**
+	 * Assumption: A list of Integer
+	 * Algorithm: find the length of the list, then minus k to get the index.
+	 * Complexity: O(N) to iterate the whole list.
+	 */
+	public static SinglyLinkedList returnKthToLast(SinglyLinkedList head, int k) {
 		if (k < 0 || head == null)
 			return null;
 		SinglyLinkedList node = head;
@@ -26,5 +32,16 @@ public class Solution22 {
 			i++;
 		}
 		return node;
+	}
+	
+	public static void main(String[] args) {
+		SinglyLinkedList head1 = new SinglyLinkedList(1);
+		head1.next =  new SinglyLinkedList(2);
+		head1.next.next =  new SinglyLinkedList(3);
+		head1.next.next.next =  new SinglyLinkedList(4);
+		System.out.println("the list: 1 -> 2 -> 3 -> 4");
+		for (int k = 0; k < 4; k++) {
+			System.out.println("the " + k + "-th node to the last is " + returnKthToLast(head1, k).value);
+		}
 	}
 }
--- ch2/src/Solution22Test.java
+++ ch2/src/Solution22Test.java
@@ -10,12 +10,12 @@ public class Solution22Test {
 		head1.next =  new Solution22.SinglyLinkedList(2);
 		head1.next.next =  new Solution22.SinglyLinkedList(3);
 		head1.next.next.next =  new Solution22.SinglyLinkedList(4);
-		assertEquals(Solution22.ReturnKthToLast(null, 0), null);
-		assertEquals(Solution22.ReturnKthToLast(null, 1), null);
-		assertEquals(Solution22.ReturnKthToLast(head1, 0).value, 4);
-		assertEquals(Solution22.ReturnKthToLast(head1, 1).value, 3);
-		assertEquals(Solution22.ReturnKthToLast(head1, 2).value, 2);
-		assertEquals(Solution22.ReturnKthToLast(head1, 3).value, 1);
-		assertEquals(Solution22.ReturnKthToLast(head1, 4), null);
+		assertEquals(Solution22.returnKthToLast(null, 0), null);
+		assertEquals(Solution22.returnKthToLast(null, 1), null);
+		assertEquals(Solution22.returnKthToLast(head1, 0).value, 4);
+		assertEquals(Solution22.returnKthToLast(head1, 1).value, 3);
+		assertEquals(Solution22.returnKthToLast(head1, 2).value, 2);
+		assertEquals(Solution22.returnKthToLast(head1, 3).value, 1);
+		assertEquals(Solution22.returnKthToLast(head1, 4), null);
 	}
 }

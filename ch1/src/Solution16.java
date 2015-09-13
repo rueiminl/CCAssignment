ed4797b27d5629c92803a49b8efe2d82eee72d01
rueiminl <rueiminl@andrew.cmu.edu>
2015-09-13 01:30:51 -0400

    1.6 String Compression: add main()

--- ch1/src/Solution16.java
+++ ch1/src/Solution16.java
@@ -1,5 +1,9 @@
-
 public class Solution16 {
+	/**
+	 * Assumption: only uppercase and lowercase letters (given by the question)
+	 * Algorithm: iterate the string and count the frequency of each continuous character
+	 * Complexity: O(N) where N is the length of the string
+	 */
 	public static String stringCompression(String str) {
 		if (str.isEmpty())
 			return "";
@@ -23,5 +27,14 @@ public class Solution16 {
 			return str;
 		else
 			return result.toString();
+	}
+	
+	public static void main(String[] args) {
+		String[] strs = {"", "a", "aa", "aaa", "aaaaaaaaaa", "aaaaaaaaaabBBB"};
+		for (String str : strs) {
+			String comp = stringCompression(str);
+			System.out.println("\"" + str + "\" is compressed to \"" + comp + "\"");
+		}
+		// expectation: "aa" -> "a2" which is the same length with original string, so return "aa"
 	}
 }

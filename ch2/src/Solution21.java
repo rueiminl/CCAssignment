8bd8303dd28c634b04c1991e601950c36da616f7
rueiminl <rueiminl@andrew.cmu.edu>
2015-09-13 09:02:31 -0400

    2.1 Remove Dups: add main()

--- ch2/src/Solution21.java
+++ ch2/src/Solution21.java
@@ -5,6 +5,11 @@ import java.util.ListIterator;
 import java.util.Set;
 
 public class Solution21 {
+	/**
+	 * Algorithm: store all existed node in a set
+	 * Complexity: O(N) to iterate all the nodes in the list (insert and check a hash set cost O(1))
+	 * Space Complexity: O(N) (happens if all nodes have different value) 
+	 */
 	public static void removeDups(LinkedList<Integer> head) {
 		Iterator<Integer> it = head.iterator();
 		Set<Integer> exist = new HashSet<Integer>();
@@ -16,6 +21,13 @@ public class Solution21 {
 				exist.add(i);
 		}
 	}
+	
+	/**
+	 * Follow Up: without additional data structure.
+	 * Algorithm: two iteration to check all pairs of nodes in the link. remove second one if duplicated.
+	 * Complexity: O(N^2)
+	 * Space Complexity: O(1)
+	 */
 	public static void removeDups2(LinkedList<Integer> head) {
 		// follow up: without additional memory
 		for (int index = 0; index < head.size(); index++) {
@@ -28,7 +40,45 @@ public class Solution21 {
 			}
 		}
 	}
+	public static void print(LinkedList<Integer> head) {
+		Iterator<Integer> iter = head.iterator();
+		if (iter.hasNext())
+			System.out.print(iter.next());
+		while (iter.hasNext()) {
+			System.out.print(" -> ");
+			System.out.print(iter.next());
+		}
+		System.out.println("");
+	}
 	public static void main(String[] args) {
+		LinkedList<Integer> list1 = new LinkedList<Integer>();
+		list1.add(1);
+		list1.add(2);
+		list1.add(3);
+		list1.add(2);
+		list1.add(4);
+		list1.add(5);
+		list1.add(2);
+		list1.add(6);
+		list1.add(7);
+		list1.add(7);
+		list1.add(6);
+		@SuppressWarnings("unchecked")
+		LinkedList<Integer> list2 = (LinkedList<Integer>) list1.clone();
 		
+		System.out.println("Before:");
+		print(list1);
+
+		removeDups(list1);
+		System.out.println("After:");
+		print(list1);
+
+		System.out.println("FOLLOW UP");
+		System.out.println("Before:");
+		print(list2);
+
+		removeDups2(list2);
+		System.out.println("After:");
+		print(list2);
 	}
 }
--- ch2/src/Solution21Test.java
+++ ch2/src/Solution21Test.java
@@ -25,6 +25,43 @@ public class Solution21Test {
 		LinkedList<Integer> listWithoutDups1 = new LinkedList<Integer>();
 		for (Integer i = 1; i <= 7; i++)
 			listWithoutDups1.add(i);
+		Solution21.removeDups(list1);
+		assertTrue(list1.equals(listWithoutDups1));
+
+		LinkedList<Integer> list2 = new LinkedList<Integer>();
+		list2.add(1);
+		list2.add(1);
+		list2.add(1);
+		list2.add(1);
+		list2.add(1);
+		LinkedList<Integer> listWithoutDups2 = new LinkedList<Integer>();
+		listWithoutDups2.add(1);
+		Solution21.removeDups(list2);
+		assertTrue(list2.equals(listWithoutDups2));
+
+		LinkedList<Integer> list3 = new LinkedList<Integer>();
+		LinkedList<Integer> listWithoutDups3 = new LinkedList<Integer>();
+		Solution21.removeDups(list3);
+		assertTrue(list3.equals(listWithoutDups3));
+	}
+
+	@Test
+	public void testFollowUp() {
+		LinkedList<Integer> list1 = new LinkedList<Integer>();
+		list1.add(1);
+		list1.add(2);
+		list1.add(3);
+		list1.add(2);
+		list1.add(4);
+		list1.add(5);
+		list1.add(2);
+		list1.add(6);
+		list1.add(7);
+		list1.add(7);
+		list1.add(6);
+		LinkedList<Integer> listWithoutDups1 = new LinkedList<Integer>();
+		for (Integer i = 1; i <= 7; i++)
+			listWithoutDups1.add(i);
 		Solution21.removeDups2(list1);
 		assertTrue(list1.equals(listWithoutDups1));
 
@@ -44,5 +81,4 @@ public class Solution21Test {
 		Solution21.removeDups2(list3);
 		assertTrue(list3.equals(listWithoutDups3));
 	}
-
 }

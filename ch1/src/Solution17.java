7f6f414624a87128e6d72563b5fda9a436d84433
rueiminl <rueiminl@andrew.cmu.edu>
2015-09-13 01:39:21 -0400

    1.7 Rotate Matrix: add main()

--- ch1/src/Solution17.java
+++ ch1/src/Solution17.java
@@ -1,6 +1,15 @@
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
 
 public class Solution17 {
-	public static void rotateMatrix(int [][] matrix) {
+	/**
+	 * Assumption: clockwise rotation
+	 * Assumption: matrix is a square (N x N)
+	 * Algorithm: rotate all points in the right-top region (including the centered vertical line)
+	 * Complexity: O(N^2)
+	 */
+	public static void rotateMatrix(int[][] matrix) {
 		int N = matrix.length;
 		for (int i = 0; i < (N + 1) / 2; i++) {
 			for (int j = 0; j < N / 2; j++) {
@@ -11,5 +20,25 @@ public class Solution17 {
 				matrix[j][N-i-1] = temp;
 			}
 		}
+	}
+	
+	public static void print(int[][] matrix) {
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				System.out.print(matrix[i][j] + " ");
+			}
+			System.out.println("");
+		}
+	}
+	public static void main(String[] args) {
+		int [][] matrix = new int [3][3];
+		for (int i = 0; i < 9; i++)
+			matrix[i/3][i%3] = i + 1;
+		System.out.println("before:");
+		print(matrix);
+		
+		rotateMatrix(matrix);
+		System.out.println("after:");
+		print(matrix);
 	}
 }

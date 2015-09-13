c3174795ded1a8f97efc865c49aaa703fb0fc24b
rueiminl <rueiminl@andrew.cmu.edu>
2015-09-13 10:14:40 -0400

    2.4 Partition: add main()

--- ch2/src/Solution24.java
+++ ch2/src/Solution24.java
@@ -1,3 +1,4 @@
+
 public class Solution24 {
 	public static class SinglyLinkedList {
 		public SinglyLinkedList(int value) {
@@ -7,6 +8,13 @@ public class Solution24 {
 		public int value;
 		public SinglyLinkedList next;
 	}
+	
+	/**
+	 * To rearrange the list so that all nodes with smaller value than x put in the front of the list
+	 * Algorithm: iterate the whole list, store smaller nodes and other nodes in separated place.
+	 *   After that, merge them.
+	 * Complexity: O(N) to iterate the whole list.
+	 */
 	public static SinglyLinkedList partition(SinglyLinkedList head, int x) {
 		SinglyLinkedList smallerHead = new SinglyLinkedList(0);
 		SinglyLinkedList smallerTail = smallerHead;
@@ -29,5 +37,40 @@ public class Solution24 {
 		biggerOrEqualTail.next = null;
 		smallerTail.next = biggerOrEqualHead.next;
 		return smallerHead.next;
+	}
+
+	public static void print(SinglyLinkedList head) {
+		if (head == null)
+			return;
+		System.out.print(head.value);
+		SinglyLinkedList node = head.next;
+		while (node != null) {
+			System.out.print(" -> ");
+			System.out.print(node.value);
+			node = node.next;
+		}
+		System.out.println("");
+	}
+	
+	public static void main(String[] args) {
+		SinglyLinkedList node = new SinglyLinkedList(3);
+		SinglyLinkedList head = node;
+		node.next = new SinglyLinkedList(5);
+		node = node.next;
+		node.next = new SinglyLinkedList(8);
+		node = node.next;
+		node.next = new SinglyLinkedList(5);
+		node = node.next;
+		node.next = new SinglyLinkedList(10);
+		node = node.next;
+		node.next = new SinglyLinkedList(2);
+		node = node.next;
+		node.next = new SinglyLinkedList(1);
+		
+		System.out.println("Before:");
+		print(head);
+		System.out.println("After:");
+		head = partition(head, 5);
+		print(head);
 	}
 }

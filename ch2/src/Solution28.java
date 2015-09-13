cba1eb7c34d33d7268ab7514663e2eadc3e07837
rueiminl <rueiminl@andrew.cmu.edu>
2015-09-13 11:20:41 -0400

    2.8 Loop Detection: add main()

--- ch2/src/Solution28.java
+++ ch2/src/Solution28.java
@@ -1,3 +1,4 @@
+
 public class Solution28 {
 	public static class SinglyLinkedList {
 		public SinglyLinkedList(int value) {
@@ -7,6 +8,17 @@ public class Solution28 {
 		public int value;
 		public SinglyLinkedList next;
 	}
+	
+	/**
+	 * Assumption: input is a list of Integer.
+	 * To find the beginning of the loop in a linked list.
+	 * Algorithm: Use a slow pointer and a fast pointer. If there is a loop, they will meet somewhere in the loop.
+	 *   The tricky part is to find where the beginning of the loop is.
+	 *   The idea is illustrated (commented) below.
+	 * Complexity: O(N) for iterating the list.
+	 * @param head the target list.
+	 * @return null if no loop; otherwise, a node in the beginning of the loop.
+	 */
 	public static SinglyLinkedList loopDetection(SinglyLinkedList head) {
 		SinglyLinkedList slow = head;
 		SinglyLinkedList fast = head;
@@ -31,5 +43,25 @@ public class Solution28 {
 			slow = slow.next;
 		}
 		return node;
+	}
+	
+	public static void main(String[] args) {
+		SinglyLinkedList node1 = new SinglyLinkedList(1);
+		SinglyLinkedList node2 = new SinglyLinkedList(2);
+		SinglyLinkedList node3 = new SinglyLinkedList(3);
+		SinglyLinkedList node4 = new SinglyLinkedList(4);
+		SinglyLinkedList node5 = new SinglyLinkedList(5);
+		node1.next = node2;
+		node2.next = node3;
+		node3.next = node4;
+		node4.next = node5;
+		node5.next = node3;
+		SinglyLinkedList head = node1;
+		
+		System.out.println("Input:");
+		System.out.println("1->2->3->4->5");
+		System.out.println("      ^-----+");
+		SinglyLinkedList result = loopDetection(head);
+		System.out.println("Output:" + result.value);
 	}
 }

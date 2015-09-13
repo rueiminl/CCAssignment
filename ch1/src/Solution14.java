96a925ec44d67c17b4dc0385ac770018925712bf
rueiminl <rueiminl@andrew.cmu.edu>
2015-09-12 23:14:59 -0400

    1.4 Palindrome Permutation: add main()

--- ch1/src/Solution14.java
+++ ch1/src/Solution14.java
@@ -1,6 +1,14 @@
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class Solution14 {
 	public static boolean palindromePermutation(String str) {
+		/**
+		 * assumption: str contains alphabet and space only (i.e. [a-zA-Z ])
+		 * algorithm: count frequency of each alphabet;
+		 *   if 2 or more alphabet are odd, then it's impossible to have a palindrome permutation.
+		 * complexity: O(N)
+		 */
 		// check if all chars are pair; note: str should contain letters only
 		boolean [] single = new boolean [26];
 		for (int i = 0; i < str.length(); i++) {
@@ -22,5 +30,17 @@ public class Solution14 {
 				count++;
 		}
 		return count <= 1;
+	}
+	public static void main(String[] args) {
+		String[] strs = {"", "a", "abcba", "aAbBcCd", "aaaaBBbbCcCcddDD", "_", "1", "aaabbb"};
+		for (String str : strs) {
+			if (palindromePermutation(str)) {
+				System.out.println("\"" + str + "\" is a permutation of a palindrome");
+			}
+			else {
+				System.out.println("\"" + str + "\" is not a permutation of a palindrome");
+			}
+		}
+		// expectation: True, True, True, True, True, False (not alphabet), False (not alphabet), False (no permutation could be a palindrome)
 	}
 }

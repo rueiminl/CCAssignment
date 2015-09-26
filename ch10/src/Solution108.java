b815abe1f078e696a03db2baa869254d3a1767b1
rueiminl <rueiminl@andrew.cmu.edu>
2015-09-25 21:49:35 -0400

    10.8 find duplicates

--- ch10/src/Solution108.java
+++ ch10/src/Solution108.java
@@ -1,4 +1,7 @@
 
 public class Solution108 {
-
+	public static void main(String[] args) {
+		String myImplementation = "Since I've got 4KB memory, I could have 4000 bit.\nIn this case, I'd like to run 8 iteration; the first iteration to seek if there is any duplicated number in the range from 0 to 4000; the second iteration (needed only if the first iteration can not find any) find duplicated number in the range from 4000 to 8000; and so on.";
+		System.out.println(myImplementation);
+	}
 }

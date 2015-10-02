868b92c74d75bd57d7e54c4ddbb0a8be33b931ab
rueiminl <rueiminl@andrew.cmu.edu>
2015-10-02 15:36:54 -0400

    6.3 dominos

---
+++ ch6/src/Solution63.java
@@ -0,0 +1,10 @@
+
+public class Solution63 {
+	public static void main(String[] args) {
+		StringBuffer mySolution = new StringBuffer();
+		mySolution.append("No, you can't. Consider a domino has a while square and a black square.\n");
+		mySolution.append("31 dominoes would have 31 white squares and 31 black squares.\n");
+		mySolution.append("Since the chessboard has 30 whilte squares and 32 black squares left, it's impossible to do that.\n");
+		System.out.println(mySolution.toString());
+	}
+}
